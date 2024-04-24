cfab48d9620103dd32a85caa5c2d14177f18e442
KinggKong <134825851+KinggKong@users.noreply.github.com>
2024-04-24 10:15:30 +0700

    Init Project

---
+++ src/main/java/org/example/assshoes/AssShoesApplication.java
@@ -0,0 +1,13 @@
+package org.example.assshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssShoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssShoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/assshoes/AssShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssShoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
